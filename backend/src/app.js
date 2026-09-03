db55b0cdf49011c0448aba19b3a743c86903b573
jagdep-singh <jagdeep.singh12a@gmail.com>
2026-09-03 17:54:57 +0530

    Added endpoint for product fetching and improve 404 error handling

--- backend/src/app.js
+++ backend/src/app.js
@@ -9,13 +9,20 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-
+app.get("/api", (req, res) => {
+  res.status(200).json({ message: "go to /api/products to fetch products" });
+});
 
 app.get("/api/health", (req, res) => {
   res.status(200).json({ message: "ok" });
 });
 
 app.use("/api/products" , productRoutes);
+
+app.use((req, res) => {
+  res.status(404).json({ error: `Route not found: ${req.method} ${req.originalUrl}` });
+});
+
 
 app.use(errorHandler);
 export default app;
